dfd82d590e098938e01399c0fbbf5136a118b75f
GALA <milliondr@naver.com>
2022-05-13 22:38:31 +0900

    Add global style and router

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import Router from "./Router";
+import GlobalStyle from "./GlobalStyle";
+import { ReactQueryDevtools } from "react-query/devtools";
+
+function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <Router />
+      <ReactQueryDevtools initialIsOpen={true} />
+    </>
+  );
+}
+
+export default App;
